c9e1e315c6e2da957b457e9fbf89ed149c67f20d
atsuko fukui <muumuumuumu@gmail.com>
2018-12-23 16:09:55 +0900

    initial commit

---
+++ app/src/main/java/com/example/muumuu/ruletextview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.muumuu.ruletextview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
